254d4d66bda9b456eef7aa4623eeb18cc81f3ec1
racheland <younghe_lim@hotmail.com>
2018-04-24 22:45:40 +0900

    강아지 몸무게 서버 올리기
    -

--- block.rb
+++ block.rb
@@ -7,9 +7,7 @@ class Dog
 	end
 
 	def my_weight
-	 puts "기존 몸무게 :" + @previous_weight.to_s 
-	 puts "현재 몸무게 :" + @weight.to_s
-
+	 	@weight.to_s
 	end
 
  def eat 
@@ -17,23 +15,5 @@ class Dog
  	@weight = @weight + 0.5
  	puts "냠냠"
  end
-
- def run
- 	puts "왈왈"
- end
-
- def walk
- 	puts "헥헥"
- end
-
-aaaaaaaa = Dog.new
-bb = Dog.new
-
-aaaaaaaa.my_weight
-aaaaaaaa.eat
-
-aaaaaaaa.my_weight
-
-bb.my_weight
 
 end
--- server.rb
+++ server.rb
@@ -1,16 +1,16 @@
 # encoding: UTF-8
 
 require 'sinatra'
+require './block'
 
-get '/join' do
-	"<h1>회원가입 페이지 입니다.</h1>"
-end
+aaaaaaaa = Dog.new
+
 
 get '/' do
-	"dog"
+	"dog" + aaaaaaaa.my_weight
 end
 
-get '/logout' do
-	'로그아웃 하셨습니다.'
+get '/eat' do
+	
 end
 
