a3dbe7931e6943c451040c13c8af620de2a9c85d
racheland <younghe_lim@hotmail.com>
2018-04-24 22:35:29 +0900

    기존몸무게 추가
    몸무게 2개 들고있음

--- block.rb
+++ block.rb
@@ -2,15 +2,18 @@ class Dog
 
 	def initialize 
 		@weight = 0.5
+		@previous_weight = 0
 		@age = 1
 	end
 
 	def my_weight
+	 puts "기존 몸무게 :" + @previous_weight.to_s 
 	 puts "현재 몸무게 :" + @weight.to_s
 
 	end
 
  def eat 
+ 	@previous_weight = @weight
  	@weight = @weight + 0.5
  	puts "냠냠"
  end
